b54c417a738e1cfa2bdc5a810dae32c36a609980
joshuacyriacus <joshuacyriacus8@gmail.com>
2025-11-20 00:49:33 +0100

    deploy change

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 4011
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
 app.use(express.json())
 app.use(cors({
@@ -18,7 +18,7 @@ app.use(cors({
 if ( process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
-    app.get("*", (req, res) => {
+    app.get("/", (req, res) => {
        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
     })     
 }
